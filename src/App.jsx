ccffc160b081a543626f53677a88f7ce7acfe63a
Nick Kang <nickkang257@gmail.com>
2024-09-19 13:32:37 -0700

    Fetches dogs from The Dog API to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import HomePage from './pages/HomePage';
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<HomePage />}>
-
+      <Route>
+        <Route path='/' element={<HomePage />}>
+        </Route>
       </Route>
     )
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function HomePage() {
+  const url = `https://api.thedogapi.com/v1/images/search?limit=2`;
+  const apiKey = 'live_AmI0cgHhcix28uSaJUsNBXzBT7a4XtYZit1ngCj7PwqvNsWHOT5Rg794pU27yJQ3';
+
+  const [dogs, setDogs] = useState([]);
+  const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetching random dog image from The Dog API
+    fetch(url, {
+      headers: {
+        'x-api-key': apiKey
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setDogs(data);
+      })
+      .catch(error => {
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <div>
       HomePage
+      <ul>
+        {dogs.map((dog) => (
+          <li key={dog.id}>
+            {dog.name}
+            <img src={dog.url} style={{ width: '150px', height: '150px' }}/>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
